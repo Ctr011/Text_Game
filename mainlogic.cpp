af56b5c7e476034f981ad513bc63cf30695cbcd6
Cal Trujillo <102545014+Ctr011@users.noreply.github.com>
2024-03-22 23:07:34 -0700

    Initial Files
    Starting files for the new project I am working on..,. Currently need to think of the story before getting to program the system.

---
+++ mainlogic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+/*
+    This is the main logic for the game. Need to figure out what the game is going to do before using this logic. 
+
+*/
---
+++ scene.h
@@ -0,0 +1,10 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+#include <stdio.h>
+
+void intro(){
+    printf("The year 1995... a year to remember... all the adventures I had with my friends and the trouble I got into...\n");
+    printf("But there is an event from that year which will never leave my mind... something that keeps appearing in my dreams over and over again...\n");
+}
+
+#endif
