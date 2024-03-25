fbdee45909a614163ad0e0143105e7c663237dee
Cal Trujillo <102545014+Ctr011@users.noreply.github.com>
2024-03-25 16:35:44 -0700

    Updating
    Just updating what the file is about

---
+++ speech.h
@@ -0,0 +1,8 @@
+#ifndef __SPEECH_H__
+#define __SPEECH_H__
+
+/*
+    The speech portion of the text game. This is the place where we can store the lines for characters we may introduce. 
+*/
+
+#endif
