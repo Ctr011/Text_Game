4f3e5465f35dcc5c9cd22450bb5626679f414811
Cal Trujillo <102545014+Ctr011@users.noreply.github.com>
2024-05-13 16:48:28 -0700

    Small
    Small updates to the scenes now... Created the skeleton of the bad endings.

--- scene.h
+++ scene.h
@@ -2,9 +2,51 @@
 #define __SCENE_H__
 #include <stdio.h>
 
-void intro(){
+void prologue(){
     printf("The year 1995... a year to remember... all the adventures I had with my friends and the trouble I got into...\n");
     printf("But there is an event from that year which will never leave my mind... something that keeps appearing in my dreams over and over again...\n");
+    printf("\n\n");
+    printf("\n\n");
+    printf("I need to get my head straight to tell this story...");
+    printf("");
+}
+
+void act1_intro(){
+    printf("");
+}
+
+void act2_intro(){
+    printf("");
+}
+
+void act3_intro(){
+    printf("");
+}
+
+void act4_intro(){
+    printf("");
+}
+
+void bad_end1(){ // Ending where the main protagonist loses hope...
+    printf("Sometimes, I wonder if, there is a God, but tonight, I see there is none...\n");
+
+    printf("BAD ENDING 1");
+}
+
+
+void bad_end2(){ // Everyone he worked hard to save is dead...
+    printf("We should not have taken this adventure...I am sorry my dear friends...\n");
+
+    printf("BAD ENDING 2");
+}
+
+void bad_end3(){ //...
+    printf("\n");
+
+    printf("BAD ENDING 3");
+}
+void epologue(){
+    printf("");
 }
 
 #endif
